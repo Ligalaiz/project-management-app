f8d754acc1a791dfb46fc0b0fda8b0e5054b691c
Ligalaiz <slavakuz88@gmail.com>
2022-05-16 09:07:58 +0300

    style: BR-1 add Board styles

---
+++ src/components/Board/Board.style.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const board = css`
+  display: flex;
+  max-height: 320px;
+  padding: 0;
+  gap: 5px;
+
+  overflow-x: scroll;
+  overflow-y: hidden;
+`;
