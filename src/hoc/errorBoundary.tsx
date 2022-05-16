da07ac69bc3d28caa2d8fe51eebb26e0d76e9e4b
Ligalaiz <slavakuz88@gmail.com>
2022-05-16 09:27:59 +0300

    feat: BR-1 implement errorBoundary hoc

---
+++ src/hoc/errorBoundary.tsx
@@ -0,0 +1,14 @@
+import { ComponentType } from 'react';
+import { withErrorBoundary } from 'react-error-boundary';
+
+import { errorFallback } from '@utils/errorFallback.utils';
+
+const errorBoundary = <P extends object>(Component: ComponentType<P>) =>
+  withErrorBoundary(Component, {
+    FallbackComponent: errorFallback,
+    onError(error) {
+      console.log({ error });
+    },
+  });
+
+export { errorBoundary };
