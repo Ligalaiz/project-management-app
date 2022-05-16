e803f32d5bd69a6617707f869345575cb3eed1cf
Ligalaiz <slavakuz88@gmail.com>
2022-05-16 10:02:58 +0300

    refactor: BR-1 import provider and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React, { FC } from 'react';
+import { Provider } from 'react-redux';
+import { AppRouter } from '@src/router';
+import { store } from '@src/store';
 
 const App: FC = () => {
-  return <div />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 };
 
 export { App };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
-import { Global } from '@emotion/react';
-
 import React from 'react';
+import { Global } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import { globalStyle } from '@src/styles';
-
 import { App } from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
