ed9a77b309de955010a11bdcc8f59256dd6fab1f
Ligalaiz <slavakuz88@gmail.com>
2022-05-22 20:09:03 +0300

    refactor: BR-1 change getColumn utils

--- src/utils/getColumn.utils.ts
+++ src/utils/getColumn.utils.ts
@@ -1,24 +1,18 @@
 import { IGetColumn } from '@src/store/board/Board.types';
 
-const getColumn = ({ target, columns, boardPosition }: IGetColumn) => {
-  const countColumns = columns.length;
-  const columnWidth = Math.round((boardPosition?.width || 0) / countColumns);
+const getColumn = ({ target, columnsPosition }: IGetColumn) => {
+  let result = null;
 
-  const columnsPositionArr = [...Array(countColumns)].map((_, ind) => ({
-    width: columnWidth,
-    top: boardPosition?.top ?? 0,
-    right:
-      ind === 0
-        ? (boardPosition?.left || 0) + columnWidth
-        : (boardPosition?.left || 0) + 5 * ind + columnWidth * (ind + 1),
-    left: ind === 0 ? boardPosition?.left || 0 : (boardPosition?.left || 0) + (5 + columnWidth) * ind,
-    bottom: boardPosition?.bottom ?? 0,
-  }));
+  columnsPosition.forEach((item) => {
+    const {
+      id,
+      position: { left, right },
+    } = item;
 
-  let result = 0;
-  columnsPositionArr.forEach((item, ind) => {
-    if (target >= item.left && target <= item.right) {
-      result = ind;
+    if (left && right) {
+      if (target >= left && target <= right) {
+        result = id;
+      }
     }
   });
   return result;
