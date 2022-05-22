88303490b5effec2adc99c610da734e253113589
Ligalaiz <slavakuz88@gmail.com>
2022-05-22 20:07:21 +0300

    refactor: BR-1 add Board reducers

--- src/store/board/Board.reducer.ts
+++ src/store/board/Board.reducer.ts
@@ -2,52 +2,92 @@ import { PayloadAction } from '@reduxjs/toolkit';
 
 import {
   IInitialBoardState,
-  SetPositionColumnsAction,
   IColumns,
-  ITask,
+  ISetTask,
   SetBoardPositionAction,
   IReorder,
   SetErrorAction,
+  IColumnPosition,
+  IRemoveTask,
+  IEditColumn,
+  IEditTask,
 } from './Board.types';
 
-interface IRemoveTask {
-  id: number;
-  status: string;
-}
-
-interface ISetTask {
-  task: ITask;
-  status: string;
-}
-
 const boardReducer = {
-  setPositionColumns: (state: IInitialBoardState, action: PayloadAction<SetPositionColumnsAction>) => {
-    state.columsPosition = [...state.columsPosition, action.payload.columsPosition];
+  setPositionColumns: (state: IInitialBoardState, action: PayloadAction<IColumnPosition[]>) => {
+    state.columnsPosition = [...action.payload];
   },
+
   setPositionBoard: (state: IInitialBoardState, action: PayloadAction<SetBoardPositionAction>) => {
     state.boardPosition = action.payload.boardPosition;
   },
+
   reorderColumns: (state: IInitialBoardState, action: PayloadAction<IColumns[]>) => {
     state.columns = [...action.payload];
   },
 
   reorderTasks: (state: IInitialBoardState, action: PayloadAction<IReorder>) => {
-    const { tasks, type } = action.payload;
-    state.tasks[type] = [...tasks];
+    const { tasks, id } = action.payload;
+    const ind = state.columns.findIndex((item) => item.id === id);
+
+    state.columns[ind].items = [...tasks];
+  },
+
+  removeColumn: (state: IInitialBoardState, action: PayloadAction<number>) => {
+    state.columns = [...state.columns.filter((column) => column.id !== action.payload)];
+  },
+
+  editColumn: (state: IInitialBoardState, action: PayloadAction<IEditColumn>) => {
+    const { columnEdit, columnId } = action.payload;
+
+    state.columns = [
+      ...state.columns.map((column) => {
+        if (column.id === columnId) {
+          return { ...column, ...columnEdit };
+        }
+        return column;
+      }),
+    ];
   },
 
   removeTask: (state: IInitialBoardState, action: PayloadAction<IRemoveTask>) => {
-    const { id, status } = action.payload;
-    state.tasks[status] = state.tasks[status].filter((item) => item.id !== id);
+    const { taskId, columnId } = action.payload;
+    const ind = state.columns.findIndex((item) => item.id === columnId);
+
+    state.columns[ind].items = [...state.columns[ind].items.filter((item) => item.id !== taskId)];
   },
 
   setTask: (state: IInitialBoardState, action: PayloadAction<ISetTask>) => {
-    const { task, status } = action.payload;
-    state.tasks[status] = [task, ...state.tasks[status]];
+    const { task, targetColumn } = action.payload;
+    const ind = state.columns.findIndex((item) => item.id === targetColumn);
+
+    state.columns[ind].items = [{ ...task, type: state.columns[ind].type }, ...state.columns[ind].items];
   },
 
-  setDragStartPosition: (state: IInitialBoardState, action: PayloadAction<string>) => {
-    state.dragStartPosition = action.payload;
+  editTask: (state: IInitialBoardState, action: PayloadAction<IEditTask>) => {
+    const { task, targetTask, targetColumn } = action.payload;
+    const ind = state.columns.findIndex((item) => item.id === targetColumn);
+
+    state.columns[ind].items = [
+      ...state.columns[ind].items.map((item) => {
+        if (item.id === targetTask) {
+          return { ...item, ...task };
+        }
+        return item;
+      }),
+    ];
+  },
+
+  setTaskCount: (state: IInitialBoardState, action: PayloadAction<number>) => {
+    state.tasksCount = action.payload;
+  },
+
+  setColumnCount: (state: IInitialBoardState, action: PayloadAction<number>) => {
+    state.columnCount = action.payload;
+  },
+
+  setColumn: (state: IInitialBoardState, action: PayloadAction<IColumns>) => {
+    state.columns = [...state.columns, action.payload];
   },
 
   setError: (state: IInitialBoardState, action: PayloadAction<SetErrorAction>) => {
