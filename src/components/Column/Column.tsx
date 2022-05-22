7dbba31468e1a74f6f457b5af808fc9050e705fb
Ligalaiz <slavakuz88@gmail.com>
2022-05-22 20:16:21 +0300

    refactor: BR-1 add Modal popup

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,48 +1,126 @@
-import React, { FC, useRef } from 'react';
-import { Reorder } from 'framer-motion';
+import React, { ChangeEvent, useState, MouseEvent } from 'react';
+import { Reorder, useDragControls } from 'framer-motion';
 import { color } from '@src/styles';
 import { Tasks } from '@components/Tasks';
-import { useTypedUseSelector } from '@src/hooks/useTypedUseSelector';
-import { IColumns } from '@src/store/board/Board.types';
+import { IDefaultColumns } from '@src/store/board/Board.types';
+import { Modal } from '@components/Modal';
+import { Submenu } from '@components/Submenu';
+import { useAction } from '@src/hooks/useAction';
 import * as c from './Column.style';
 
-interface IDefaultColumns {
-  column: IColumns;
-}
-const Column: FC<IDefaultColumns> = ({ column }) => {
-  const { tasks } = useTypedUseSelector((state) => state.board);
-  const ref = useRef<HTMLDivElement>(null);
+const Column = React.forwardRef((props: IDefaultColumns, ref) => {
+  const { column, handleDrag } = props;
+  const { editColumn } = useAction();
+  const [isModal, SetIstModal] = useState(false);
+  const [isSubmenu, SetIsSubmenu] = useState(false);
+  const [isTitle, setIsTitle] = useState(false);
+  const [modalType, setModalType] = useState('');
+  const [titleValue, setTitleValue] = useState(column!.title || '');
+  const controls = useDragControls();
+
+  const handleCloseModal = (e: MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
+    handleCloseSubmenu(e);
+    const {
+      dataset: { type },
+    } = e.target as typeof e.target & {
+      dataset: {
+        type: string;
+      };
+    };
+
+    if (type === 'close') {
+      SetIstModal(!isModal);
+      setModalType('atask');
+    }
+    if (type === 'columnEdit') {
+      SetIstModal(true);
+      setModalType('ecolumn');
+    }
+    if (type === 'columnDelete') {
+      SetIstModal(true);
+      setModalType('dcolumn');
+    }
+  };
+
+  const handleCloseSubmenu = (e: MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
+    const {
+      dataset: { type },
+    } = e.target as typeof e.target & {
+      dataset: {
+        type: string;
+      };
+    };
+
+    if (type === 'submenu') {
+      SetIsSubmenu(!isSubmenu);
+    } else {
+      SetIsSubmenu(false);
+    }
+  };
+
+  const handleClickTitle = () => {
+    setIsTitle(true);
+  };
+
+  const handleBlur = () => {
+    if (titleValue?.length > 3 && column) {
+      const columnEdit = {
+        title: titleValue,
+        type: titleValue,
+      };
+      editColumn({ columnEdit, columnId: column.id });
+    }
+    setIsTitle(false);
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const castEvent = event as unknown as ChangeEvent<HTMLInputElement>;
+    setTitleValue(castEvent.target.value);
+  };
+
+  const dragStyle = {
+    border: '1px solid blue',
+    borderColor: `${color['blue-color']}`,
+    boxShadow: `0 0 0 0.2rem ${color['blue-color-light-X1']}`,
+  };
 
   return (
-    <Reorder.Item
-      value={column}
-      css={c.columnList}
-      style={{ minWidth: '353px' }}
-      whileDrag={{
-        border: '1px solid blue',
-        borderColor: `${color['blue-color']}`,
-        boxShadow: `0 0 0 0.2rem ${color['blue-color-light-X1']}`,
-      }}
-    >
-      <div css={c.boardWrap} ref={ref}>
-        <div css={c.titleWrap}>
-          <div css={c.title}>
-            <div css={c.taskCountWrap}>
-              <span css={c.taskCount}>{tasks?.[column?.type].length ?? 0}</span>
+    <>
+      <Reorder.Item
+        value={column}
+        css={c.columnList}
+        drag
+        ref={ref}
+        dragListener={false}
+        dragControls={controls}
+        whileDrag={dragStyle}
+        onDragEnd={handleDrag}
+      >
+        <div css={c.boardWrap}>
+          <div css={c.titleWrap} onPointerDown={(e) => controls.start(e)}>
+            <div css={c.title}>
+              <div css={c.taskCountWrap}>
+                <span css={c.taskCount}>{column?.items.length}</span>
+              </div>
+              {!isTitle && <div onClick={handleClickTitle}>{column.title}</div>}
+              {isTitle && (
+                <input css={c.columnTitle} type="text" value={titleValue} onBlur={handleBlur} onChange={handleChange} />
+              )}
             </div>
-            <div>{column.title}</div>
+            <div css={c.menu}>
+              <button type="button" css={c.addBtn} onClick={handleCloseModal} data-type="close">
+                +
+              </button>
+              <button type="button" data-type="submenu" css={c.menuBtn} onClick={handleCloseSubmenu} />
+              {isSubmenu && <Submenu target="column" handleClose={handleCloseModal} />}
+            </div>
           </div>
-          <div css={c.menu}>
-            <button type="button" css={c.addBtn}>
-              +
-            </button>
-            <button type="button" css={c.menuBtn} />
-          </div>
+          <Tasks column={column} />
         </div>
-        <Tasks column={column} />
-      </div>
-    </Reorder.Item>
+      </Reorder.Item>
+      {isModal && <Modal type={modalType} handleClose={handleCloseModal} column={column} />}
+    </>
   );
-};
+});
 
 export { Column };
