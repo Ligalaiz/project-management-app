4d043be1378840236c6c4addf48a9c7b49486b93
Ligalaiz <slavakuz88@gmail.com>
2022-05-22 20:08:13 +0300

    style: BR-1 change global styles

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -4,8 +4,10 @@ interface IColor {
 
 export const color: IColor = {
   'white-color': '#ffffff',
+  'white-color-X1': '#f0f6fc1a',
   'white-color-dark': '#bcd1d9',
   'black-color': '#010409',
+  'black-color-X1': '#00000099',
   'gray-color': '#0d1117',
   'gray-color-light': '#161b22',
   'gray-color-light-X1': '#30363d',
@@ -13,7 +15,14 @@ export const color: IColor = {
   'gray-color-light-X3': '#2d3138',
   'gray-color-light-X4': '#6d7580',
   'gray-color-light-X5': '#c9d1d9',
+  'gray-color-light-X6': '#545a65',
+  'gray-color-light-X8': '#21262d',
+  'gray-color-light-X9': '#838c96',
   'blue-color': '#1f6feb',
   'blue-color-light': '#58a6ff',
   'blue-color-light-X1': '#1f6feb30',
+  'green-color': '#238636',
+  'green-color-X1': '#2ea043',
+  'red-color': '#f85149',
+  'red-color-X1': '#da3633',
 };
