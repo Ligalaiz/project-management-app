fb0f864f9993fcf5ab02d62510fd3f62cdaac8e0
Ligalaiz <slavakuz88@gmail.com>
2022-05-22 20:14:28 +0300

    refactor: BR-1 add Modal component

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useRef } from 'react';
+import React, { MouseEvent, FC, useRef, useState } from 'react';
 import { Reorder } from 'framer-motion';
 import { color } from '@src/styles';
-import { ITaskItem } from '@src/store/board/Board.types';
+import { ITaskItem, IHandleDragEnd } from '@src/store/board/Board.types';
 import { useAction } from '@src/hooks/useAction';
 import { getColumn } from '@src/utils';
+import { Submenu } from '@components/Submenu';
 import { useTypedUseSelector } from '@src/hooks/useTypedUseSelector';
+import { Modal } from '@components/Modal';
 import * as t from './TaskItem.style';
 
 const variants = {
@@ -13,55 +15,103 @@ const variants = {
   exit: { opacity: 0, height: 0 },
 };
 
-const TaskItem: FC<ITaskItem> = ({ task }) => {
-  const { boardPosition, dragStartPosition, columns } = useTypedUseSelector((state) => state.board);
-  const { removeTask, setDragStartPosition, setTask } = useAction();
+const TaskItem: FC<ITaskItem> = ({ task, column }) => {
+  const { boardPosition, columnsPosition } = useTypedUseSelector((state) => state.board);
+  const { removeTask, setTask } = useAction();
+  const [isSubmenu, SetIsSubmenu] = useState(false);
+  const [isModal, SetIsModal] = useState(false);
+  const [modalType, setModalType] = useState('');
   const ref = useRef<HTMLDivElement>(null);
 
+  const handleCloseSubmenu = (e: MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
+    const {
+      dataset: { type },
+    } = e.target as typeof e.target & {
+      dataset: {
+        type: string;
+      };
+    };
+
+    if (type === 'submenu') {
+      SetIsSubmenu(!isSubmenu);
+    } else {
+      SetIsSubmenu(false);
+    }
+  };
+
+  const handleCloseModal = (e: MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
+    handleCloseSubmenu(e);
+    const {
+      dataset: { type },
+    } = e.target as typeof e.target & {
+      dataset: {
+        type: string;
+      };
+    };
+
+    if (type === 'close') {
+      SetIsModal(!isModal);
+    }
+    if (type === 'taskEdit') {
+      SetIsModal(true);
+      setModalType('etask');
+    }
+    if (type === 'taskDelete') {
+      SetIsModal(true);
+      setModalType('dtask');
+    }
+  };
+
+  const handleDragEnd = (event: globalThis.MouseEvent | TouchEvent | PointerEvent, info: IHandleDragEnd) => {
+    const {
+      point: { x, y },
+    } = info;
+
+    if (boardPosition) {
+      const { left, right, top, bottom } = boardPosition;
+
+      if (x >= left && x <= right && y >= top && y <= bottom) {
+        const targetColumn = getColumn({ target: x, columnsPosition });
+
+        if ((targetColumn || targetColumn === 0) && column.id !== targetColumn) {
+          removeTask({ taskId: task.id, columnId: column.id });
+          setTask({ task, targetColumn });
+        }
+      }
+    }
+  };
+
   return (
-    <Reorder.Item
-      value={task}
-      css={t.task}
-      drag
-      whileDrag={{
-        position: 'absolute',
-        border: '1px solid blue',
-        borderColor: `${color['blue-color']}`,
-        boxShadow: `0 0 0 0.2rem ${color['blue-color-light-X1']}`,
-      }}
-      onDragStart={() => {
-        const rect = ref?.current?.getBoundingClientRect();
-
-        if (boardPosition && rect) {
-          const indColumn = getColumn({ target: rect.x, columns, boardPosition });
-          setDragStartPosition(columns[indColumn]?.type);
-        }
-      }}
-      onDragEnd={() => {
-        const rect = ref?.current?.getBoundingClientRect();
-
-        if (boardPosition && rect && dragStartPosition) {
-          const indColumn = getColumn({ target: rect.x, columns, boardPosition });
-
-          if (columns[indColumn].type !== dragStartPosition) {
-            removeTask({ id: task.id, status: dragStartPosition });
-            setTask({ task: { ...task, status: columns[indColumn]?.type }, status: columns[indColumn]?.type });
-          }
-        }
-      }}
-      {...variants}
-    >
-      <div css={t.taskMenu} ref={ref}>
-        <div css={t.taskTitle}>{task.title}</div>
-        <button type="button" css={t.taskMenuBtn} />
-      </div>
-      <div css={t.executorWrap}>
-        Add by{' '}
-        <a href="/#" css={t.executor}>
-          Ligalaiz
-        </a>
-      </div>
-    </Reorder.Item>
+    <>
+      <Reorder.Item
+        value={task}
+        css={t.task}
+        drag
+        whileDrag={{
+          zIndex: '1000',
+          position: 'absolute',
+          border: '1px solid blue',
+          borderColor: `${color['blue-color']}`,
+          boxShadow: `0 0 0 0.2rem ${color['blue-color-light-X1']}`,
+        }}
+        onDragEnd={handleDragEnd}
+        {...variants}
+      >
+        <div css={t.taskMenu} ref={ref}>
+          <div css={t.taskTitle}>{task.title}</div>
+          <button type="button" data-type="submenu" css={t.taskMenuBtn} onClick={handleCloseSubmenu} />
+          {isSubmenu && <Submenu target="task" handleClose={handleCloseModal} />}
+        </div>
+        <div>{task.content}</div>
+        <div css={t.executorWrap}>
+          Add by{' '}
+          <a href="/#" css={t.executor}>
+            Ligalaiz
+          </a>
+        </div>
+      </Reorder.Item>
+      {isModal && <Modal type={modalType} handleClose={handleCloseModal} column={column} task={task} />}
+    </>
   );
 };
 
