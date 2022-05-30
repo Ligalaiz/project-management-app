4f5b645b3bb3dc793bd57dfe6f09a9e18a415ff1
Ligalaiz <slavakuz88@gmail.com>
2022-05-30 13:35:49 +0300

    style: BR-3 add Footer styles

---
+++ src/components/Footer/Footer.style.ts
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+import { color } from '@src/styles';
+
+export const footerWrap = css`
+  flex: 0 0 auto;
+  margin-top: 2rem;
+
+  background-color: ${color['gray-color-light']};
+`;
+
+export const footer = css`
+  display: flex;
+  justify-content: space-around;
+  padding: 20px 0;
+`;
+
+export const footerLink = css`
+  color: ${color['white-color']};
+`;
