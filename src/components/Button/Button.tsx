443e92533c5dea6629c15dfcf4e8d192b6f551a9
Ligalaiz <slavakuz88@gmail.com>
2022-06-03 20:32:58 +0300

    refactor: BR-4 add type condition

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { useTypedUseSelector } from '@src/hooks/useTypedUseSelector';
 import { IButton } from '@src/store/board/Board.types';
 import * as b from './Button.style';
 
-const Button: FC<IButton> = ({ type, title, handleClick, dataType, dataName }) => {
+const Button: FC<IButton> = ({ type, title, handleClick, dataType, dataName, disabled }) => {
   const { filter, sortType } = useTypedUseSelector((state) => state.board);
   let btnStyle = b.btn;
   let classN = '';
@@ -15,6 +15,7 @@ const Button: FC<IButton> = ({ type, title, handleClick, dataType, dataName }) =
   if (type === 'submenuSort') btnStyle = b.submenuSort;
   if (type === 'openCount') btnStyle = b.openCount;
   if (type === 'closeCount') btnStyle = b.closeCount;
+  if (type === 'start') btnStyle = b.start;
 
   if (dataType === 'filterProject') {
     if (type === 'openCount' && !filter) {
@@ -28,7 +29,15 @@ const Button: FC<IButton> = ({ type, title, handleClick, dataType, dataName }) =
   if (dataType === sortType) classN = 'sortActive';
 
   return (
-    <button className={classN} css={btnStyle} onClick={handleClick} data-type={dataType} data-name={dataName}>
+    <button
+      type={type === 'submit' ? 'submit' : 'button'}
+      className={classN}
+      css={btnStyle}
+      onClick={handleClick}
+      data-type={dataType}
+      data-name={dataName}
+      disabled={disabled}
+    >
       {title}
     </button>
   );
